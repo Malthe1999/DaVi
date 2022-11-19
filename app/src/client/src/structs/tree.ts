a03940d98191eccfaab0c51867e46be90a4be09a
Ivan Luchev <luchevz@gmail.com>
2022-11-19 19:34:43 +0100

    Add tree structure

--- app/src/client/src/structs/tree.ts
+++ app/src/client/src/structs/tree.ts
@@ -1,12 +1,20 @@
+import { getThemeProps } from "@mui/system";
+
 export class Tree {
   root: Node;
   pointers: { [key: string]: Node };
 
-  constructor() {
-    this.root = { value: "Cluster", parent: undefined, children: [] };
-    this.pointers = {
-      "Cluster": this.root,
+  constructor(rootName: string) {
+    this.root = {
+      name: rootName,
+      parent: undefined,
+      children: [],
+      gProps: {
+        className: "custom node",
+      },
     };
+    this.pointers = {};
+    this.pointers[rootName] = this.root;
   }
 
   insert(parent: string | any, child: string) {
@@ -22,22 +30,32 @@ export class Tree {
     }
 
     const childNode: Node = {
-      value: child,
+      name: child,
       parent: this.pointers[parent],
       children: [],
     };
     this.pointers[parent].children.push(childNode);
   }
 
-  addEdge(parent: string, child: string) {
-    // Add a possibly disconnected edge in the tree
+  addEdge(parent: string, child: string, highlighted: boolean, onClick: any) {
     if (this.pointers[child] === undefined) {
-      this.pointers[child] = { value: child, parent: undefined, children: [] };
+      this.pointers[child] = {
+        name: child,
+        parent: undefined,
+        children: [],
+        highlighted: highlighted,
+        gProps: {
+          className: highlighted ? "custom node highlighted" : "custom node",
+          onClick: () => {
+            onClick(child);
+          },
+        },
+      };
     }
 
     if (this.pointers[parent] === undefined) {
       this.pointers[parent] = {
-        value: parent,
+        name: parent,
         parent: undefined,
         children: [this.pointers[child]],
       };
@@ -47,10 +65,45 @@ export class Tree {
 
     this.pointers[child].parent = this.pointers[parent];
   }
+
+  highlightParents() {
+    for (const child of Object.keys(this.pointers)) {
+      if (this.pointers[child].highlighted) {
+        let parent = this.pointers[child].parent;
+        while (parent && parent.gProps) {
+          parent.highlighted = true;
+          parent.gProps["className"] = "custom node highlighted";
+          parent = parent.parent;
+        }
+      }
+    }
+  }
+
+  getHighlighted() {
+    const result: string[] = [];
+    for (const node of Object.values(this.pointers)) {
+      if (node.highlighted) {
+        result.push(node.name ?? "");
+      }
+    }
+    return result;
+  }
+
+  getParents(child: string) {
+    const result: string[] = [];
+    let parent = this.pointers[child].parent;
+    while (parent) {
+      result.push(parent.name ?? "");
+      parent = parent.parent;
+    }
+    return result;
+  }
 }
 
 interface Node {
-  value: string;
+  name?: string;
   parent: Node | undefined;
   children: Node[];
+  gProps?: any;
+  highlighted?: boolean;
 }
